5674dda4531e32d18073419445cb3e83d8c2d1bc
techrwt <rwt.daily.blogs@gmail.com>
2026-07-29 13:39:54 +0530

    Update database.js

--- firebase/database.js
+++ firebase/database.js
@@ -1,1 +1,26 @@
+import { db } from "./config.js";
 
+import {
+collection,
+addDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+async function saveQuestion(data){
+
+try{
+
+await addDoc(collection(db,"questions"),data);
+
+alert("Question Submitted Successfully!");
+
+}catch(error){
+
+console.error(error);
+
+alert("Error submitting question.");
+
+}
+
+}
+
+export { saveQuestion };
