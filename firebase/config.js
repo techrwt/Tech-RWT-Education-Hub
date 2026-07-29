6cf96d275c0917da86cceef5af0dd248ca3eceff
techrwt <rwt.daily.blogs@gmail.com>
2026-07-29 17:57:17 +0530

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,17 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY"
+  apiKey: "YOUR_API_KEY_HERE",
   authDomain: "tech-rwt-education-hub.firebaseapp.com",
   projectId: "tech-rwt-education-hub",
-  storageBucket: "tech-rwt-education-hub.firebasestorage.app",
-  messagingSenderId: "883411092191",
-  appId: "1:883411092191:web:8d6d96b29034cecf119f6a"
+  storageBucket: "tech-rwt-education-hub.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
+// Export db so other pages can use it
 export { db };
