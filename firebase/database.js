966c501dc3ebec0ee4bb278e1c229bd0126e5156
techrwt <rwt.daily.blogs@gmail.com>
2026-07-29 14:58:18 +0530

    Update database.js

--- firebase/database.js
+++ firebase/database.js
@@ -1,37 +1,26 @@
 import { db } from "./config.js";
+import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-import {
-collection,
-addDoc
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-async function saveQuestion(data){
-
-try{
-
-await addDoc(collection(db,"questions"),data);
-
-alert("Question Submitted Successfully!");
-
-}catch(error){
-
-console.error(error);
-
-alert("Error submitting question.");
-
+async function saveQuestion(data) {
+  try {
+    await addDoc(collection(db, "questions"), data);
+    alert("Question Submitted Successfully!");
+  } catch (error) {
+    console.error(error);
+    alert("Error submitting question.");
+  }
 }
-
-}
-
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 async function getQuestions() {
   const snapshot = await getDocs(collection(db, "questions"));
   const questions = [];
   snapshot.forEach((doc) => {
-    questions.push({ id: doc.id, ...doc.data() });
+    questions.path({
+      id: doc.id,
+      ...doc.data()
+    });
   });
   return questions;
 }
 
-export { saveQuestion, getQuestions };
+export { db, saveQuestion, getQuestions };
