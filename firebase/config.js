3d408eac0d1cc7548d5dae9d8759136f5f40f35e
techrwt <rwt.daily.blogs@gmail.com>
2026-07-29 13:39:24 +0530

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD3S1wg31KRNbFPm483H-JZH1aFSUV4Org",
+  authDomain: "tech-rwt-education-hub.firebaseapp.com",
+  projectId: "tech-rwt-education-hub",
+  storageBucket: "tech-rwt-education-hub.firebasestorage.app",
+  messagingSenderId: "883411092191",
+  appId: "1:883411092191:web:8d6d96b29034cecf119f6a"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
